489889e9d27b950366d43697fae9254e2496de96
csaidane <cs.saidane@gmail.com>
2020-06-22 21:28:56 +0000

    implemented the head function

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+
+};
+
+
+const head = function(array){
+  return array[0];
+};
