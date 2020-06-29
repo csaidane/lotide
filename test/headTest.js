4a8db1cacd2074b5d36017de9ed74e06d0fe7c65
csaidane <cs.saidane@gmail.com>
2020-06-29 18:40:44 +0000

    first tests converted to mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,17 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("should return 'hello' for ['Hello', 'Lighthouse', 'Labs']", function(){
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+  })
+
+
+});
